a2f021755b3152b2bbbfe444b89cb8d3a462e7db
RohanKhatua <contact@rohankhatua.dev>
2024-11-10 22:59:54 +0530

    Add configuration class for MongoDB connection settings

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+
+class Config:
+    MONGO_USERNAME = os.getenv("MONGO_DB_USERNAME")
+    MONGO_PASSWORD = os.getenv("MONGO_DB_PASSWORD")
+    MONGO_HOST = os.getenv("MONGO_DB_HOST", "localhost")
+    MONGO_PORT = os.getenv("MONGO_DB_PORT", 27017)
+    MONGO_DB_NAME = os.getenv("MONGO_DB_NAME", "test")
+    # MONGO_URI = f"mongodb://{MONGO_USERNAME}:{MONGO_PASSWORD}@{MONGO_HOST}:{MONGO_PORT}/"
+    MONGO_URI = f"mongodb://{MONGO_USERNAME}:{MONGO_PASSWORD}@{MONGO_HOST}:{MONGO_PORT}/{MONGO_DB_NAME}?authSource=admin"
