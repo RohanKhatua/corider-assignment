1c50817486bf4be96285c9dc9aad564af7122c13
RohanKhatua <contact@rohankhatua.dev>
2024-11-10 23:00:48 +0530

    Add initial Flask application setup and .gitignore file

---
+++ main.py
@@ -0,0 +1,19 @@
+from flask import Flask, jsonify
+from app.db import db
+from app.routes.users import user_bp
+
+app = Flask(__name__)
+app.register_blueprint(user_bp)
+
+
+@app.route("/")
+def index():
+    try:
+        db.command("ping")
+        return jsonify({"status": "ok"})
+    except Exception as e:
+        return jsonify({"status": "error", "error": str(e)})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=6000, debug=True)
