d7efd8e53690be19a01b6157105e08ce3d6d542f
RohanKhatua <contact@rohankhatua.dev>
2024-11-11 11:07:30 +0530

    Enhance MongoDB connection handling and initialization in db.py and main.py

--- app/db.py
+++ app/db.py
@@ -1,11 +1,27 @@
-from pymongo import MongoClient
+from pymongo import MongoClient, errors
 from config import Config
+import logging
 
-try:
-    client = MongoClient(Config.MONGO_URI)
-    db = client[Config.MONGO_DB_NAME]
-    print("Connected to MongoDB")
-    print(db._name)
-except Exception as e:
-    print(f"Error connecting to MongoDB: {e}")
-    db = None
+log = logging.getLogger(__name__)
+
+# Declare db as None initially
+db = None
+
+
+def init_db():
+    """Initialize and set the MongoDB client connection."""
+    # log.debug("Initializing the database connection")
+    global db  # Use the global db variable
+    try:
+        log.info("Connecting to MongoDB...")
+        client = MongoClient(Config.MONGO_URI, serverSelectionTimeoutMS=5000)
+        client.server_info()  # Trigger connection attempt
+        db = client[Config.MONGO_DB_NAME]
+        log.info(f"Connected to MongoDB: {db._name}")
+        return db
+    except errors.ServerSelectionTimeoutError as e:
+        log.error("Error connecting to MongoDB: Connection timed out.")
+        raise RuntimeError(f"Error connecting to MongoDB: {e}")
+    except Exception as e:
+        log.error(f"Error connecting to MongoDB: {e}")
+        raise RuntimeError(f"Error connecting to MongoDB: {e}")
--- main.py
+++ main.py
@@ -1,5 +1,23 @@
-from flask import Flask, jsonify
-from app.db import db
+import logging
+import sys
+from app.db import init_db  # Import db so other files can access it
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+log = logging.getLogger(__name__)
+logging.getLogger("pymongo").setLevel(logging.WARNING)
+
+# Run the database check before the app starts
+try:
+    log.debug("Calling init_db() from main.py")
+    init_db()  # Initialize the database; this sets the db object in db.py
+except RuntimeError as e:
+    sys.exit(str(e))  # Exit the app with an error if the DB check fails
+
+from flask import Flask
 from app.routes.users import user_bp
 from app.routes.ping import ping_bp
 
@@ -8,14 +26,6 @@ app.register_blueprint(user_bp)
 app.register_blueprint(ping_bp)
 
 
-@app.route("/")
-def index():
-    try:
-        db.command("ping")
-        return jsonify({"status": "ok"})
-    except Exception as e:
-        return jsonify({"status": "error", "error": str(e)})
-
-
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=6000, debug=True)
+    print("Starting the app")
+    app.run(host="0.0.0.0", port=int(os.getenv("FLASK_PORT")), debug=True)
